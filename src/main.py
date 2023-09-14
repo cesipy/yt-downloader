b992f18f2245af1a4e385647b388c42150e28bb6
cedric <cedric.sillaber@gmail.com>
2023-09-14 17:28:30 +0200

    add doc

--- src/main.py
+++ src/main.py
@@ -4,17 +4,23 @@ import downloader
 import os
 
 
-def handle_cli_arguments():
+def handle_cli_arguments() -> downloader.Downloader:
+    """
+    handles the command line arguments.
+    
+    :return: dl downloader object
+    """
     length_argv = len(sys.argv)
 
     # Correct number of arguments: url only
     if length_argv == 2:
         url = sys.argv[1]
-        is_valid = valid_argument(url)
+        is_valid = valid_url(url)
 
         if is_valid:
             # path = os.path.relpath("downloaded_videos", "yt-downloader")
             path = "downloaded_videos"
+            # create downloader object
             dl = downloader.Downloader(url, path)
             return dl
         else:
@@ -24,11 +30,12 @@ def handle_cli_arguments():
     # Correct number of arguments: url and custom path
     elif length_argv == 3:
         url = sys.argv[1]
-        is_valid = valid_argument(url)
+        is_valid = valid_url(url)
         custom_path = sys.argv[2]
 
         if is_valid:
             if os.path.exists(custom_path) and os.path.isdir(custom_path):
+                # create downloader object
                 dl = downloader.Downloader(url, custom_path)
                 return dl
             else:
@@ -38,13 +45,18 @@ def handle_cli_arguments():
             print(f"Incorrect url: {url}")
             sys.exit(1)
 
-    # Incorrect usage
+    # incorrect usage
     else:
         print(f"Incorrect usage! Usage: python {sys.argv[0]} <url to video> ( <destination directory> )")
         sys.exit(1)
 
 
-def valid_argument(url):
+def valid_url(url: str) -> bool:
+    """
+    checks if the given url is correct and can be used for the downloader.
+    :param url
+    :return: bool: whether url is valid or not.
+    """
     try:
         parsed_url = urllib.parse.urlparse(url)
         if parsed_url.scheme and parsed_url.netloc:
