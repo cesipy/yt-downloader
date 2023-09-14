26e4f45793d4b4c2daac9c5e35b2ab0f3e70ba9c
cedric <cedric.sillaber@gmail.com>
2023-09-14 18:08:04 +0200

    refactor

--- src/main.py
+++ src/main.py
@@ -4,50 +4,42 @@ import downloader
 import os
 
 
-def handle_cli_arguments() -> downloader.Downloader:
+def handle_cli():
+    if len(sys.argv) == 2:
+        url = sys.argv[1]
+        dl = handle_arguments(url, "downloaded_videos")
+        return dl
+
+    elif len(sys.argv) == 3:
+        url = sys.argv[1]
+        custom_path = sys.argv[2]
+        dl = handle_arguments(url, custom_path)
+        return dl
+
+    else:
+        print(f"incorrect usage! usage: python {sys.argv[0]} <url> ( additional <output path> )")
+        sys.exit(1)
+
+
+def handle_arguments(url: str, path: str) -> downloader.Downloader:
     """
-    handles the command line arguments.
+    handles the url and path and returns a downloader object
     
     :return: dl downloader object
     """
-    length_argv = len(sys.argv)
+    is_valid = valid_url(url)
 
-    # Correct number of arguments: url only
-    if length_argv == 2:
-        url = sys.argv[1]
-        is_valid = valid_url(url)
-
-        if is_valid:
-            # path = os.path.relpath("downloaded_videos", "yt-downloader")
-            path = "downloaded_videos"
+    if is_valid:
+        if os.path.exists(path) and os.path.isdir(path):
             # create downloader object
             dl = downloader.Downloader(url, path)
             return dl
+
         else:
-            print(f"Incorrect url: {url}")
+            print(f"Custom path does not exist or is not a directory: {path}")
             sys.exit(1)
-
-    # Correct number of arguments: url and custom path
-    elif length_argv == 3:
-        url = sys.argv[1]
-        is_valid = valid_url(url)
-        custom_path = sys.argv[2]
-
-        if is_valid:
-            if os.path.exists(custom_path) and os.path.isdir(custom_path):
-                # create downloader object
-                dl = downloader.Downloader(url, custom_path)
-                return dl
-            else:
-                print(f"Custom path does not exist or is not a directory: {custom_path}")
-                sys.exit(1)
-        else:
-            print(f"Incorrect url: {url}")
-            sys.exit(1)
-
-    # incorrect usage
     else:
-        print(f"Incorrect usage! Usage: python {sys.argv[0]} <url to video> ( <destination directory> )")
+        print(f"Incorrect url: {url}")
         sys.exit(1)
 
 
@@ -68,7 +60,7 @@ def valid_url(url: str) -> bool:
 
 
 def main():
-    dl = handle_cli_arguments()
+    dl = handle_cli()
     dl.download()
 
 
